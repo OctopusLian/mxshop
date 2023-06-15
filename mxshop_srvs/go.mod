202f91b36ea225c2125f7c201647b152d18f32be
OctopusLian <zoctopus@qq.com>
2023-06-15 23:53:57 +0800

    feat(user_srv，close #16): grpc服务注册到consul中，并实现服务的健康检查

--- mxshop_srvs/go.mod
+++ mxshop_srvs/go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/anaskhan96/go-password-encoder v0.0.0-20201010210601-c765b799fd72
 	github.com/golang/protobuf v1.5.3
+	github.com/hashicorp/consul/api v1.20.0
 	github.com/spf13/viper v1.16.0
 	go.uber.org/zap v1.21.0
 	google.golang.org/grpc v1.55.0
@@ -14,12 +15,23 @@ require (
 )
 
 require (
+	github.com/armon/go-metrics v0.4.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v1.2.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hashicorp/serf v0.10.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
--- mxshop_srvs/user_srv/main.go
+++ mxshop_srvs/user_srv/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/hashicorp/consul/api"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
+	"mxshop_srvs/user_srv/global"
 	"mxshop_srvs/user_srv/handler"
 	"mxshop_srvs/user_srv/initialize"
 	"mxshop_srvs/user_srv/proto"
@@ -20,6 +23,7 @@ func main() {
 	initialize.InitLogger()
 	initialize.InitConfig()
 	initialize.InitDB()
+	zap.S().Info(global.ServerConfig)
 
 	flag.Parse()
 	fmt.Println("ip: ", *IP)
@@ -34,6 +38,35 @@ func main() {
 
 	// 注册服务健康检查
 	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
+
+	// 服务注册
+	cfg := api.DefaultConfig()
+	cfg.Address = fmt.Sprintf("%s:%d", global.ServerConfig.ConsulInfo.Host,
+		global.ServerConfig.ConsulInfo.Port)
+	client, err := api.NewClient(cfg)
+	if err != nil {
+		panic(err)
+	}
+	// 生成对应的检查对象
+	check := &api.AgentServiceCheck{
+		GRPC:                           fmt.Sprintf("127.0.0.1:50051"),
+		Timeout:                        "5s",
+		Interval:                       "5s",
+		DeregisterCriticalServiceAfter: "15s",
+	}
+	// 生成注册对象
+	registration := new(api.AgentServiceRegistration)
+	registration.Name = global.ServerConfig.Name
+	registration.ID = global.ServerConfig.Name
+	registration.Port = *Port
+	registration.Tags = []string{"mxshop", "neozhang", "user", "srv"}
+	registration.Check = check
+
+	err = client.Agent().ServiceRegister(registration)
+	if err != nil {
+		panic(err)
+	}
+
 	err = server.Serve(lis)
 	if err != nil {
 		panic("failed to start grpc:" + err.Error())
